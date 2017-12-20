9e2ca5c554b7ab1601fd0b40cd604dc3c2cb087b
joshrhughes <josh@mtnpine.us>
2017-12-20 09:25:50 -0700

    morning exercise

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+console.log('hello');
+
 function addProjects() {
-	var projectDiv = document.getElementById("#ourProjects");
+	var projectDiv = document.getElementById("ourProjects");
 	for (var i =0; i < 4; i++) {
 		var project = document.createElement("div");
 		var projectLink = document.createElement("a");
 		var projectImage = document.createElement("img");
-		projectLink.setAttribute("href", "http://www.zombo.com");
-		projectImage.setAttribute("src", "project"+1+".png");
+		//projectLink.setAttribute("href", "http://www.zombo.com");
+		projectImage.setAttribute("src", "project"+i+".png");
 		projectLink.appendChild(projectImage);
 		project.appendChild(projectLink);
 		projectDiv.appendChild(project);
